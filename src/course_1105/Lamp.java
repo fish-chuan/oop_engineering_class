ac8ecdfe083cc18dc7cb4e7b04d8fb15521a9c01
yu-chuan <newdavidlin@gmail.com>
2020-11-05 09:58:17 +0800

    Add 1105

---
+++ src/course_1105/Lamp.java
@@ -0,0 +1,13 @@
+package course_1105;
+
+public class Lamp {
+    private boolean is_turnOn = false;
+
+    public void TurnOn() {
+        is_turnOn = true;
+    }
+
+    public void TurnOff() {
+        is_turnOn = false;
+    }
+}
